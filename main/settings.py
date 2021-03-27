8422ea7ddbc3d90813c02caa52b71d71059dcd72
J Young Kim <jyoungk@andrew.cmu.edu>
2021-03-27 15:29:19 -0400

    Has an App. Changed to 'main'

--- main/settings.py
+++ main/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gifApp.apps.GifappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'gifMyDay.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gifMyDay.wsgi.application'
+WSGI_APPLICATION = 'main.wsgi.application'
 
 
 # Database
