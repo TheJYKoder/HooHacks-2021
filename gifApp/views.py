190ec7dadcf6f9de82b5de3417992d90808337d6
Ascoli <bga3xb@virginia.edu>
2021-03-28 00:18:14 -0400

    some nonworking registration stuff

--- gifApp/views.py
+++ gifApp/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages #import messages
+
 
 def home(request):
     return render(request, "gifApp/home.html")
 
 def login(request):
     return render(request, "gifApp/login.html")
+
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request)
+			messages.success(request, "Registration successful." )
+			return redirect("home")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm
+	return render (request=request, template_name="gifApp/register.html", context={"register_form":form})
